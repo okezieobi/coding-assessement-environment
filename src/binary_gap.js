d2cefb59025b308b449ef6a4c81f6e4cb5f19d57
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-02-21 20:04:11 +0100

    binary gap completed

---
+++ src/binary_gap.js
@@ -0,0 +1,21 @@
+function findLongestBinaryGap(N) {
+  if (typeof N !== 'number' || /^\+?\d+$/.test(`${N}`) === false) throw new TypeError('Argument must be positive integer');
+  else if (N < 1 || N > 647) throw new RangeError('Argument must be within 1 to 647');
+  const binary = (N).toString(2);
+  const arrayResult = [];
+  let count = 0;
+  const array = binary.split('');
+  array.forEach((value, index) => {
+    if (index > 0) {
+      if (value === '0') {
+        count += 1;
+      } else if (value === '1') {
+        if (count > 0) arrayResult.push(count);
+        count = 0;
+      }
+    }
+  });
+  return arrayResult.length > 0 ? Math.max(...arrayResult) : 0;
+}
+
+console.log(findLongestBinaryGap(809));
