3753bf8c01875eeb328149f6e0598dd03bcd88ea
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-02-24 19:03:01 +0100

    testing

---
+++ src/leetcode_blind75/coin_change.js
@@ -0,0 +1,54 @@
+/*
+Coin Change
+Medium
+You are given an integer array coins representing coins of different denominations and
+an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+Example 1:
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+
+Example 3:
+Input: coins = [1], amount = 0
+Output: 0
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+*/
+
+function coinChange(coins = [], amount = 0) {
+  if (amount === 0) return 0;
+  let placeholderAmt = amount;
+  let fewestNoOfCoins = 0;
+  const maxCoinAmt = Math.max(...coins);
+  while (placeholderAmt >= maxCoinAmt) {
+    placeholderAmt -= maxCoinAmt;
+    fewestNoOfCoins += 1;
+  }
+  if (placeholderAmt > 0) {
+    const remainder = coins.find((val) => val === placeholderAmt);
+    if (remainder != null) {
+      placeholderAmt -= remainder;
+      fewestNoOfCoins += 1;
+    } else {
+      fewestNoOfCoins = -1;
+    }
+  }
+  return fewestNoOfCoins;
+}
+
+// eslint-disable-next-line no-console
+console.log(coinChange([1, 2147483647], 2));
--- src/leetcode_blind75/no_of_bits.js
+++ src/leetcode_blind75/no_of_bits.js
@@ -1,6 +1,6 @@
-function hammmingWeight(n) {
+function hammingWeight(n = 0) {
   return n.toString(2).split('').filter((val) => val === '1').length;
 }
 
 // eslint-disable-next-line no-console
-console.log(hammmingWeight(1011));
+console.log(hammingWeight(1011));
