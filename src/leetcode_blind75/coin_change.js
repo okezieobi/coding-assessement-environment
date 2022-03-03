e18f675edfbe35a1bfc3300e88f2e00cb14a7191
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-03-03 16:00:59 +0100

    test fix

--- src/leetcode_blind75/coin_change.js
+++ src/leetcode_blind75/coin_change.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /*
 Coin Change
 Medium
@@ -28,27 +29,41 @@ Constraints:
 1 <= coins[i] <= 231 - 1
 0 <= amount <= 104
 */
-
-function coinChange(coins = [], amount = 0) {
-  if (amount === 0) return 0;
-  let placeholderAmt = amount;
+function coinChange(coinArray = [], amt = 0) {
   let fewestNoOfCoins = 0;
-  const maxCoinAmt = Math.max(...coins);
-  while (placeholderAmt >= maxCoinAmt) {
-    placeholderAmt -= maxCoinAmt;
-    fewestNoOfCoins += 1;
-  }
-  if (placeholderAmt > 0) {
-    const remainder = coins.find((val) => val === placeholderAmt);
-    if (remainder != null) {
-      placeholderAmt -= remainder;
+  function coinChangeRunner(coins = [], amount = 0) {
+    if (amount === 0) return 0;
+    let placeholderAmt = amount;
+    const maxCoinAmt = Math.max(...coins);
+    console.log(placeholderAmt, maxCoinAmt);
+    while (placeholderAmt >= maxCoinAmt) {
+      placeholderAmt -= maxCoinAmt;
       fewestNoOfCoins += 1;
-    } else {
-      fewestNoOfCoins = -1;
     }
+    if (placeholderAmt > 0) {
+      const remainder = coins.filter((val) => val < maxCoinAmt);
+      if (remainder.length === 0) {
+        fewestNoOfCoins = -1;
+      } else {
+        console.log(fewestNoOfCoins);
+        coinChangeRunner(remainder, placeholderAmt);
+      }
+    }
+    return fewestNoOfCoins;
   }
-  return fewestNoOfCoins;
+  return coinChangeRunner(coinArray, amt);
 }
 
 // eslint-disable-next-line no-console
-console.log(coinChange([1, 2147483647], 2));
+console.log(coinChange([186, 83, 408, 419], 6249));
+
+// const coinChange = (coins, amount) => {
+//   const dp = Array(amount + 1).fill(Infinity); // This arr tells us how many coins we need for each amount.
+//   dp[0] = 0; // To make 0, we need 0 coins.
+//   for (const coin of coins) { // Check each coin
+//     for (let i = coin; i <= amount; i++) { // Iterate through the entire amount from coin
+//       dp[i] = Math.min(dp[i], dp[i - coin] + 1); // Update minimum number of needed coins.
+//     }
+//   }
+//   return dp[amount] === Infinity ? -1 : dp[amount]; // If the last element is Infinity, then we cannot make the amount.
+// };
